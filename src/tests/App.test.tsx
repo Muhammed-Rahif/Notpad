8652bd4b96171a2b3851b6a01a49097243d4dfcf
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-12-19 22:18:14 +0530

    chore(project): project setup
    basic test and files setup. assets (logo, font, icon) setup. scss
    config. boilerplate cleanup

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import "@testing-library/jest-dom";
+
+test("renders home page", () => {
+  render(<App />);
+
+  const linkElement = screen.getByText(/untitled - notepad/i);
+  expect(linkElement).toBeInTheDocument();
+});

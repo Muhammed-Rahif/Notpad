9eafbb3534da2aee2c30dbbafea146b022eaf242
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-12-23 20:53:32 +0530

    test: add test cases for 'AlertBox'

--- src/tests/AlertBox.test.tsx
+++ src/tests/AlertBox.test.tsx
@@ -7,7 +7,7 @@ import Context from "../contexts/Context";
 describe("renders home page with contents", () => {
   afterEach(cleanup);
 
-  it("should be visible when state is avaliable", () => {
+  it("should be visible or invisible when state changes", () => {
     render(
       <Context>
         <App />
@@ -25,6 +25,25 @@ describe("renders home page with contents", () => {
       (contnt, elem) => contnt.startsWith("No") && elem?.tagName === "BUTTON"
     );
     alertNoBtn.click();
+
+    expect(alertBoxComponent.style.display).toEqual("none");
+  });
+
+  it("alert close btn should work", () => {
+    render(
+      <Context>
+        <App />
+      </Context>
+    );
+
+    const existBtn = screen.getByText(/exit/i);
+    existBtn.click();
+
+    const alertBoxComponent = screen.getByTestId("alert-box-wrapper");
+    expect(alertBoxComponent.style.display).toEqual("flex");
+
+    const closeBtn = screen.getByTestId("btn-cls");
+    closeBtn.click();
 
     expect(alertBoxComponent.style.display).toEqual("none");
   });

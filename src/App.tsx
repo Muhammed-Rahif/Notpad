b8878bc62cf5ae62119e5297a4f2ab3e79c4ee6d
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-12-25 13:19:48 +0530

    chore: fixed a minor error modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AlertBox from "./components/AlertBox/AlertBox";
 import MenuBar from "./components/MenuBar/MenuBar";
-import Shortcuts from "./components/Shortcuts/ShortCuts";
+import Shortcuts from "./components/Shortcuts/Shortcuts";
 import TextArea from "./components/TextArea/TextArea";
 import TitleBar from "./components/TitleBar/TitleBar";
 import { handleOpenFileChange } from "./helpers";

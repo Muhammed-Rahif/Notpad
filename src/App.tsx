5ee31bf799997cd3a3504ab2e12ed6b51a276c20
Muhammed Rahif <73386156+Muhammed-Rahif@users.noreply.github.com>
2022-01-01 23:23:42 +0530

    feat(dark-mode): add toggling dark mode feature using css variables (#71)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AlertBox from "./components/AlertBox/AlertBox";
 import MenuBar from "./components/MenuBar/MenuBar";
 import Shortcuts from "./components/Shortcuts/Shortcuts";
 import TextArea from "./components/TextArea/TextArea";
 import TitleBar from "./components/TitleBar/TitleBar";
-import { handleOpenFileChange } from "./helpers";
+import { handleOpenFileChange, setUserPreference } from "./helpers";
+import "./App.scss";
 
 function App() {
+  useEffect(() => {
+    setUserPreference();
+  }, []);
+
   return (
     <Shortcuts>
       <TitleBar />

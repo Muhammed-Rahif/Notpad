dbd8aa408faf780e77effe0a491112b686a67c9e
Abhishek Pal <abhishek1100861@gmail.com>
2022-01-08 13:51:06 +0530

    Changed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import Context from "./contexts/Context";
 
 ReactDOM.render(
@@ -14,4 +13,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.unregister();
+

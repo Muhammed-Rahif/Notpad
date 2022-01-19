2c239afb25a1fade8db375cc09604add93915581
Muhammed Rahif <73386156+Muhammed-Rahif@users.noreply.github.com>
2022-01-19 20:07:39 +0530

    feat(status-bar): add cpu usage percentage to status bar (#3)

--- src/renderer/components/StatusBar/StatusBar.tsx
+++ src/renderer/components/StatusBar/StatusBar.tsx
@@ -1,11 +1,21 @@
 import './StatusBar.scss';
+import os from 'os-utils';
+import { useEffect, useState } from 'react';
 
 function StatusBar() {
+  const [cpuUsage, setCpuUsage] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      os.cpuUsage((usage) => setCpuUsage(Number((usage * 100).toFixed(1))));
+    }, 500);
+  }, []);
+
   return (
     <div className="status-bar">
       <p className="status-bar-field">Press F1 for help</p>
       <p className="status-bar-field">Slide 1</p>
-      <p className="status-bar-field">CPU Usage: 14%</p>
+      <p className="status-bar-field">CPU Usage: {cpuUsage}%</p>
     </div>
   );
 }

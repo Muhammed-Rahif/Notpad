88f7a34ded941bf63c65b8182705464e046d669a
Muhammed Rahif <73386156+Muhammed-Rahif@users.noreply.github.com>
2022-01-24 14:27:29 +0530

    test(contexts): update test cases using editor view context (#11)
    * test(contexts): update test cases using editor view context
    
    * chore(workflow): create workflow for test coverage report
    
    * chore(gitignore): add 'report.json'

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,14 +1,21 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import EditorViewProvider from '../renderer/contexts/EditorView';
 import App from '../renderer/App';
+
+const Application = () => (
+  <EditorViewProvider>
+    <App />
+  </EditorViewProvider>
+);
 
 describe('App component tests', () => {
   it('should render without crashing', () => {
-    expect(render(<App />)).toBeTruthy();
+    expect(render(<Application />)).toBeTruthy();
   });
 
   it('should have "window" elem in document body', async () => {
-    render(<App />);
+    render(<Application />);
     const windowElem = await screen.findByTestId('window');
     expect(windowElem).toBeInTheDocument();
   });
--- src/__tests__/TitleBar.test.tsx
+++ src/__tests__/TitleBar.test.tsx
@@ -1,11 +1,18 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import screenfull from 'screenfull';
+import EditorViewProvider from '../renderer/contexts/EditorView';
 import App from '../renderer/App';
+
+const Application = () => (
+  <EditorViewProvider>
+    <App />
+  </EditorViewProvider>
+);
 
 describe('TitleBar component tests', () => {
   it('should goto fullscreen when fullscreen btn clicks', async () => {
-    render(<App />);
+    render(<Application />);
     const fullscreenBtn = await screen.findByTestId('fullscreen-btn');
     fullscreenBtn.click();
 

a7b68595c65229b3b6d83a120781b5ad5addab60
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-02-11 12:59:06 +0530

    feat: add textarea input field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import AppHead from "@/components/Head/Head";
 import MenuBar from "@/components/MenuBar/MenuBar";
 import TitleBar from "@/components/TitleBar/TitleBar";
+import { Textarea } from "@mui/joy";
 
 export default function Home() {
   return (
@@ -9,6 +10,18 @@ export default function Home() {
 
       <TitleBar />
       <MenuBar />
+      <Textarea
+        size="lg"
+        variant="soft"
+        color="neutral"
+        sx={{
+          borderRadius: 0,
+          height: "100%",
+          shadow: "none",
+          border: "0px solid transparent",
+          "--Textarea-focusedHighlight": "rgba(0,0,0,0)",
+        }}
+      />
     </>
   );
 }

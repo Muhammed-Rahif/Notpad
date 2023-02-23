71080a66a4ac6cc267d44ae437dd6d44133ad8d3
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-02-23 22:47:34 +0530

    fix(pwa): pwa sw.js issue

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   skipWaiting: true,
   runtimeCaching,
-  buildExcludes: [/middleware-manifest\.json$/],
 });
 
 module.exports = withPWA({

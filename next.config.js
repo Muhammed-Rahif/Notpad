1a59c53785858f87dd127d9c229fede4e635a9a4
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-02-23 19:55:47 +0530

    feat(pwa): pwa enabled

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+});

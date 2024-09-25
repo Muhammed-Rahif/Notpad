12a20b078f617cba248e632934746829c8b45da5
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-09-25 20:53:25 +0530

    fix(editor): closes #164
    add CONTRIBUTING.md

--- src/lib/components/EditorTitle.svelte
+++ src/lib/components/EditorTitle.svelte
@@ -17,18 +17,22 @@
     }, 0);
   }
 
+  async function submit() {
+    const t = input.value.trim();
+    const isValidTitle = t !== '' && t.length > 0 && t.length <= 24;
+
+    if (isValidTitle) {
+      NotepadHelper.updateTitle(id, t);
+      readonly = true;
+
+      await tick(); // Ensure the DOM reflects the readonly change
+      input.blur();
+    }
+  }
+
   async function onKeydown(event: KeyboardEvent) {
     if (event.key === 'Enter') {
-      const t = input.value.trim();
-      const isValidTitle = t !== '' && t.length > 0 && t.length <= 24;
-
-      if (isValidTitle) {
-        NotepadHelper.updateTitle(id, t);
-        readonly = true;
-
-        await tick(); // Ensure the DOM reflects the readonly change
-        input.blur();
-      }
+      await submit();
     }
   }
 
@@ -37,15 +41,17 @@
   }
 </script>
 
-<input
-  bind:this={input}
-  use:autoWidth
-  on:dblclick|stopPropagation={onDbClick}
-  on:keydown|stopPropagation={onKeydown}
-  on:blur={onBlur}
-  bind:value={title}
-  type="text"
-  class="border-none bg-transparent font-semibold outline-none"
-  maxlength={24}
-  {readonly}
-/><span class="-ml-1 text-primary/50">.txt</span>
+<form on:submit|preventDefault={submit}>
+  <input
+    bind:this={input}
+    use:autoWidth
+    on:dblclick|stopPropagation={onDbClick}
+    on:blur={onBlur}
+    bind:value={title}
+    type="text"
+    class="border-none bg-transparent font-semibold outline-none"
+    maxlength={24}
+    {readonly}
+  />
+  <span class="-ml-1 text-primary/50">.txt</span>
+</form>

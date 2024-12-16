aae36cc1d5de80e02283ca14a2b564420ad8cf9f
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-12-17 00:36:42 +0530

    chore(gh-pages): updates

---
+++ assets/tauri-apps_plugin-process-CfKzG15F.js
@@ -0,0 +1,1 @@
+import{i as e}from"./tauri-apps_api-BO7E0eYi.js";async function t(i=0){await e("plugin:process|exit",{code:i})}export{t as e};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Notpad/sw.js', { scope: '/Notpad/' })})}

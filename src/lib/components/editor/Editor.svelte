eaadc641bb80b1b5ef9d330996990b6cd27a63f6
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-12-27 02:38:28 +0530

    refactor(editor): remove unnecessary fake-caret renderings

--- src/lib/components/editor/Editor.svelte
+++ src/lib/components/editor/Editor.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount, tick, untrack } from 'svelte';
+  import { onMount, tick } from 'svelte';
   import throttle from 'lodash.throttle';
   import debounce from 'lodash.debounce';
   import StatusBar from '@/components/StatusBar.svelte';
@@ -18,7 +18,7 @@
 
   let { editor }: Props = $props();
   let editorContainer: HTMLDivElement = $state(null!);
-  let quill: Quill = $state(null!);
+  let quill: Quill | undefined = $state();
   let fakeCaret: HTMLSpanElement | null = $state(null);
   let caretPosition = $state({ top: 10, left: 8, height: 24 });
   let lineNo = $state(0);
@@ -40,33 +40,39 @@
   /**
    * Update line and column numbers.
    */
-  function updateEditorData() {
-    const selection = quill.getSelection();
-    if (selection) {
-      lineNo = quill.getText().split('\n').length - 1; // quill.getLength() includes a trailing newline character
-      const text = quill.getText(0, selection.index + selection.length);
-      const lines = text.split('\n');
-      caretLineNo = lines.length;
-      caretColumnNo = lines[lines.length - 1].length + 1;
-      characterCount = quill.getLength() - 1; // quill.getLength() includes a trailing newline character
-      wordCount = quill
-        .getText()
-        .trim()
-        .split(/\s+/)
-        .filter((word) => word).length; // \s+ is RegEx for whitespace characters.
-    }
-  }
+  const updateEditorData = throttle(() => {
+    if (!quill) return;
+    // console.count('Update editor data');
+
+    const selection = quill?.getSelection();
+    if (!selection) return;
+
+    const fullPlainText = quill.getText();
+    lineNo = fullPlainText.split('\n').length - 1; // quill.getLength() includes a trailing newline character
+    const text = quill.getText(0, selection.index + selection.length);
+    const lines = text.split('\n');
+    caretLineNo = lines.length;
+    caretColumnNo = lines[lines.length - 1].length + 1;
+    characterCount = quill.getLength() - 1; // quill.getLength() includes a trailing newline character
+    wordCount = fullPlainText
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word).length; // \s+ is RegEx for whitespace characters.
+  }, 300);
 
   const resumeFakeCaretBlink = debounce(function () {
-    if (fakeCaret) fakeCaret.style.animationDuration = '1s';
-  }, 700);
+    if (fakeCaret) fakeCaret.classList.add('animate-caret-blink');
+  }, 1000);
 
   // Using requestAnimationFrame for smooth updates
   const updateCaretPosition = throttle(() => {
-    // console.count('Update caret');
-    requestAnimationFrame(async () => {
-      if (fakeCaret) fakeCaret.style.animationDuration = '0s';
+    if (!quill) return;
+    // console.count('Update caret position');
 
+    requestAnimationFrame(async () => {
+      if (fakeCaret) fakeCaret.classList.remove('animate-caret-blink');
+
+      if (!quill) return;
       if (editorContainer) {
         try {
           const caret = getCaretPosition(quill.root);
@@ -86,57 +92,36 @@
     });
   });
 
-  /**
-   * https://github.com/sveltejs/svelte/issues/9248#issuecomment-1732246774
-   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const explicitEffect = (fn: any, depsFn: () => void) => {
-    $effect(() => {
-      depsFn();
-      untrack(fn);
-    });
-  };
+  $effect(() => {
+    if ($settings) {
+      updateCaretPosAndEditorData();
 
-  explicitEffect(updateCaretPosition, () => [
-    editorContainer,
-    quill,
-    lineNo,
-    caretLineNo,
-    caretColumnNo,
-    wordCount,
-    characterCount,
-    $settings
-  ]);
+      // Some settings are animated as default duration 300ms. So waiting them
+      // to complete that the element are moved to its correct position for
+      // calculating the caret's correct position.
+      const timeoutId = setTimeout(updateCaretPosAndEditorData, 300);
 
-  explicitEffect(
-    async () => {
-      await tick();
-      // If line numbers are enabled or disabled, update the caret position after a delay
-      // of 300ms to complete the line numbers animated entry or exit.
-      await new Promise((resolve) => setTimeout(resolve, 300));
-      updateCaretPosition();
-    },
-    () => [$settings.lineNumbers, lineNo]
-  );
+      return () => clearTimeout(timeoutId);
+    }
+  });
 
-  const updateCaretPosAndEditorData = throttle(async () => {
+  const updateCaretPosAndEditorData = async () => {
     updateEditorData();
     await tick();
     updateCaretPosition();
-  });
+  };
 
   onMount(async () => {
     await setupQuill();
+    if (!quill) return;
 
     for (let e of ['input', 'scroll', 'click', 'keydown', 'focus', 'resize', 'load'])
       quill.root.addEventListener(e, updateCaretPosAndEditorData);
     for (let e of ['scroll', 'resize', 'load'])
       window.addEventListener(e, updateCaretPosAndEditorData);
+    updateCaretPosAndEditorData();
 
-    updateEditorData();
-    settings.subscribe(updateEditorData);
-    quill.on('editor-change', updateEditorData);
-
+    quill.on('editor-change', updateCaretPosAndEditorData);
     // Marks editor as not saved when text content changes.
     quill.on('text-change', () => Notpad.editors.setIsSaved(editor.id, false));
   });
@@ -145,7 +130,8 @@
 <div class="relative h-full overflow-hidden">
   <div
     class="editor-container relative flex overflow-hidden
-    rounded-none bg-transparent text-sm caret-transparent"
+    rounded-none bg-transparent text-sm caret-transparent
+    duration-300"
     style="--editor-font-family: '{$settings.fontFamily}';
     --editor-font-size: {$settings.fontSize}px;
     --editor-zoom: {$settings.zoom};
@@ -156,8 +142,8 @@
   ></div>
 
   <span
-    class="fake-caret absolute z-0 w-0.5 rounded-[.06em]
-    bg-primary"
+    class="fake-caret absolute z-0 w-0.5 animate-caret-blink
+    rounded-[.06em] bg-primary"
     bind:this={fakeCaret}
     style="top: {caretPosition.top}px;
     left: {caretPosition.left}px;
